c256db981789a5a1e06a78b86f621b5f8e303c74
Calculator_Java <chat26666@gmail.com>
2025-02-25 18:00:42 +0900

    class_calculator_v1_complete

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,23 @@
+import com.test.calculator.calculator;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        calculator calc = new calculator();
         Scanner sc = new Scanner(System.in);
-        int i,j;
+        int i, j;
         while (true) {
             try {
-                System.out.print("첫번째 정수를 입력하세요 : ");
+                System.out.print("첫번째 양의 정수를 입력하세요 : ");
                 i = sc.nextInt();
-                System.out.print("두번째 정수를 입력하세요 : ");
+                System.out.print("두번째 양의 정수를 입력하세요 : ");
                 j = sc.nextInt();
-            }
-            catch (Exception e) {
-                System.out.println("에러 정수를 입력해주십시오.");
-                System.out.println(e);
+                if (j < 0 || i < 0) {
+                    System.out.println("\n에러*** 양의 정수를 입력해주십시오.\n");
+                    continue;
+                }
+            } catch (Exception e) {
+                System.out.println("양의 정수를 입력해주십시오.");
                 sc.nextLine();
                 continue;
             }
@@ -22,34 +26,18 @@ public class Main {
             if (c.equals("exit")) {
                 System.out.println("종료합니다.");
                 break;
-            }
-            else if (c.charAt(0) == '/' && j == 0)
-            {
+            } else if (c.charAt(0) == '/' && j == 0) {
                 System.out.println("분모가 0 이 될수 없습니다. 재입력 부탁드립니다.");
-                continue;
-            }
-            else if (c.charAt(0) == '*' || c.charAt(0) == '/' || c.charAt(0) == '+' || c.charAt(0) == '-') {
-                switch (c.charAt(0)) {
-                    case '+':
-                        System.out.println(i + " + " + j + " = " + (i + j));
-                        break;
-                    case '-':
-                        System.out.println(i + " - " + j + " = " + (i - j));
-                        break;
-                    case '*':
-                        System.out.println(i + " * " + j + " = " + (i * j));
-                        break;
-                    case '/':
-                        System.out.println(i + " / " + j + " = " + (i / j));
-                        break;
-                    default:
-                        System.out.println("잘못된 사칙연산 기호 입력");
-                }
+            } else if (c.charAt(0) == '*' || c.charAt(0) == '/' || c.charAt(0) == '+' || c.charAt(0) == '-') {
+                calc.setter(calc.calc(i, j,c.charAt(0)));
             }
             else {
                 System.out.println("제대로 된 사칙연산 기호를 입력부탁드립니다.(+,*,/,-)");
             }
 
         }
+        calc.getter();
+        calc.remove();
+        calc.getter();
     }
 }
