6e842075f78fda04d40c82470356fd7a58c5f30c
Calculator_Java <chat26666@gmail.com>
2025-02-26 21:21:24 +0900

    refactor:변수ì이름을 좀더 보기쉽게 변경

--- src/com/test/calculator/operator.java
+++ src/com/test/calculator/operator.java
@@ -34,10 +34,10 @@ public enum operator {
     //public abstract <T extends Number> T calc(T i, T j);
 
     // 람다식 표현을 통해 코드를 좀 더 간결하게
-    ADD("+", (i, j) -> i.doubleValue() + j.doubleValue()),
-    SUB("-", (i, j) -> i.doubleValue() - j.doubleValue()),
-    MUL("*", (i, j) -> i.doubleValue() * j.doubleValue()),
-    DIV("/", (i, j) -> i.doubleValue() / j.doubleValue());
+    ADD("+", (num1, num2) -> num1.doubleValue() + num2.doubleValue()),
+    SUB("-", (num1, num2) -> num1.doubleValue() - num2.doubleValue()),
+    MUL("*", (num1, num2) -> num1.doubleValue() * num2.doubleValue()),
+    DIV("/", (num1, num2) -> num1.doubleValue() / num2.doubleValue());
 
     private final BiFunction<Double,Double,Double> calc;
     private final String op;
@@ -48,9 +48,9 @@ public enum operator {
     public String getOp() {
         return op;
     }
-    public <T extends Number> T calc(T i, T j) {
-        Double result = calc.apply(i.doubleValue(), j.doubleValue());
-        System.out.println(i + " " + op + " " + j + " = " + result);
+    public <T extends Number> T calc(T num1, T num2) {
+        Double result = calc.apply(num1.doubleValue(), num2.doubleValue());
+        System.out.println(num1 + " " + op + " " + num2 + " = " + result);
         return (T) result;
     }
 
