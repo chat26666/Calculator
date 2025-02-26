9607cfcb15aedf9242b8b6f089285dfafed99016
Calculator_Java <chat26666@gmail.com>
2025-02-26 22:51:39 +0900

    refactor : AI 기능을 활용해서 method, class 명을 기깔나게 변경

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,10 @@
-import com.test.calculator.calculator;
-import com.test.calculator.input;
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import com.test.calculator.CalculatorInputHandler;
+
 
 public class Main {
     public static void main(String[] args) {
-        input data = new input();
-        data.number_input(); //계산기 실행 메소드
-        data.bigger_number_input(); //연산 결과 값중 입력된 값보다 더 큰 값들을 출력하는 메소드
-        //calc.remove();
-        //calc.getter();
+        CalculatorInputHandler inputHandler = new CalculatorInputHandler();
+        inputHandler.handleArithmeticInput();
+        inputHandler.handleThresholdInput();
     }
 }
